861a3ec715b2a9b25001c874f6ef4e66b0cd7f79
John-Dig <120290438+John-Dig@users.noreply.github.com>
2023-02-07 16:08:48 -0800

    Initial commit

---
+++ __tests__/template.test.js
@@ -0,0 +1,7 @@
+// import Object from './../src/TEMPLATE.js';
+// describe('OBJECT', () => {
+// });
+// test('DESCRIBE TEST', () => {
+//   const VARIABLE = new OBJECT();
+//   expect(VARIABLE.THING).toEqual();
+// });
